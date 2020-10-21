80070cc3de29aa916fbfeedb408b55c0b4932e7e
Daniel Carpenter <dbry1024@gmail.com>
2020-10-21 16:39:04 -0500

    create stubs for JUNIT test

---
+++ src/SuperStackTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SuperStackTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testSuperStack() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsFull() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPush() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
