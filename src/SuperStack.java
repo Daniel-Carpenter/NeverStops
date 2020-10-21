642975e7edbdf1bb205da4bbe37527b387e275f8
Daniel Carpenter <dbry1024@gmail.com>
2020-10-21 16:38:46 -0500

    Create ss method stubs

---
+++ src/SuperStack.java
@@ -0,0 +1,30 @@
+
+public class SuperStack<T> 
+{
+	private T[] items;
+	private int top;
+	
+	public SuperStack(int size) {
+		items = new (T[]) new Object();
+	}
+	
+	boolean isEmpty() {
+		// TODO
+		return false;
+	}
+	
+	boolean isFull() {
+		// TODO
+		return false;
+	}
+	
+	boolean push(T item) {
+		// TODO
+		return true;
+	}
+	
+	public T pop() {
+		// TODO
+		return null;
+	}
+}
